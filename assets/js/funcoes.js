ec462d0760e521cbcab4e9ad9eb62816e1ac1565
PriscilaCS <39917483+SuayMack@users.noreply.github.com>
2020-09-20 01:48:02 -0300

    evento click

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,21 +1,83 @@
+/*
 function execBtn(value){
     switch (value) {
-      
+      case 'ativo':
+
+      break;
+    case 'cancelado':
+
+      break;
     }
   }
+    */
   
+  function addEventListenerAll(element, events, fn){
+    events.split(' ').forEach(event => {
+      element.addEventListener(event, fn, false)
   
+    })
+  }    
+
   
+function myFunction() { 
+
+  let span_status = document.querySelectorAll("#row_status > span")
+  console.log(span_status)
+
+  span_status.addEventListener('click', e=>{
+    console.log(e)
+  })
+
+
+
+
+/*
+  span_status.forEach()
+
   
-  
-  
-  function myFunction() { 
-  
-    let status = document.querySelectorAll("#status")
+
+    let status = document.querySelectorAll("#row_status").value
+    console.log(status)
+     
+   
+    //var textbtn = (status.outerText)
+    //console.log(textbtn)
+    /*
+    status.forEach((elem, index)=>{
+          var textbtn = (elem.innerHTML)
+          console.log(textbtn)
+    
+          
+    })
+    
+    
+    
+    
+    
+    /*
     status.forEach((elem, index)=>{
       this.addEventListenerAll( elem, "click drag", e => {
         var textbtn = (elem.innerHTML)
-        this.execBtn(textbtn)
+        console.log(textbtn)
+  
+        textbtn.forEach((stat, index)=>{
+            if (stat = 'ativo'){
+                console.log('está ativo')
+            }else{
+                console.log('está inativo')
+            }
+        })
+  
+      })
+    })
+
+    status.forEach(stts=>{
+
+        stts.addEventListener('click', e => {
+            console.log(e)
+        })
+        
+    })
   
         /*
         if (this.textbtn == 'ativo'){
@@ -23,11 +85,9 @@ function execBtn(value){
         }else{
           document.getElementById("status").innerHTML = ("ativo")
         }
-        */
+        
   
       })
-    })
-   
-    
-  }
+    */
+}
   
